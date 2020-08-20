770f6e882b661a79cbe4cf4a289c5344e5c2b56b
Ali Taleghani <ali.taleghani1@gmail.com>
2020-08-20 15:16:42 -0700

    Updates

---
+++ precache-manifest.a1ecbc310537e1630e4019fe57967a8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30051096c02ee8039fc5ded0adc3d52a",
+    "url": "/react-website-deploy/index.html"
+  },
+  {
+    "revision": "19a36eaadfbac90f1f4a",
+    "url": "/react-website-deploy/static/css/main.2f44bf53.chunk.css"
+  },
+  {
+    "revision": "461e01ee5e4e27e2c2c0",
+    "url": "/react-website-deploy/static/js/2.6b4b25c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-website-deploy/static/js/2.6b4b25c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19a36eaadfbac90f1f4a",
+    "url": "/react-website-deploy/static/js/main.6eeb937b.chunk.js"
+  },
+  {
+    "revision": "5d2ea7e41c14838b6e7d",
+    "url": "/react-website-deploy/static/js/runtime-main.57286b67.js"
+  },
+  {
+    "revision": "0a0b853a1e1b8a87119392fedb500937",
+    "url": "/react-website-deploy/static/media/mainpic.0a0b853a.jpg"
+  }
+]);
